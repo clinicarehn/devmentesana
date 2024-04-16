0b24f222c72aa98c60fd77f965fc4475e5d48a63
Edwin Velasquez <105985102+clinicarehn@users.noreply.github.com>
2024-04-16 12:29:10 -0600

    Update getMovimientosTabla.php

--- php/movimientos/getMovimientosTabla.php
+++ php/movimientos/getMovimientosTabla.php
@@ -11,18 +11,12 @@ $fechai = $_POST['fechai'];
 $fechaf = $_POST['fechaf'];
 
 //CONSULTA LOS DATOS DE LA ENTIDAD CORPORACION
-$consulta = "SELECT p.nombre AS 'producto', p.concentracion AS 'concentracion', me.nombre AS 'medida', m.cantidad_entrada AS 'entrada', m.cantidad_salida AS 'salida', m.saldo AS 'saldo', m.fecha_registro AS 'fecha_registro', m.comentario AS 'comentario', CONCAT(sf.prefijo,'',LPAD(f.number, sf.relleno, 0)) AS 'numero'
+$consulta = "SELECT p.nombre AS 'producto', p.concentracion AS 'concentracion', me.nombre AS 'medida', m.cantidad_entrada AS 'entrada', m.cantidad_salida AS 'salida', m.saldo AS 'saldo', m.fecha_registro AS 'fecha_registro', m.comentario AS 'comentario','' AS 'numero'
 	FROM movimientos AS m
 	INNER JOIN productos AS p
 	ON m.productos_id = p.productos_id
 	INNER JOIN medida AS me
 	ON p.medida_id = me.medida_id
-	LEFT JOIN facturas_detalle AS fd
-	ON fd.productos_id = m.productos_id
-	LEFT JOIN facturas AS f 
-	ON f.facturas_id = fd.facturas_id
-	LEFT JOIN secuencia_facturacion AS sf
-	ON f.secuencia_facturacion_id = sf.secuencia_facturacion_id
 	WHERE p.categoria_producto_id = '$categoria'";
 
 // Agregar la condición del producto_id si está definido
